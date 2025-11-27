d50bfea60fef1f7dad12d33c50bbcd2be8992cfe
Zamiralov Grigorij <grigorijzamiralov@gmail.com>
2025-11-27 10:12:28 +0300

    Update Number4.java

--- Number4.java
+++ Number4.java
@@ -5,24 +5,25 @@ import java.io.IOException;
 public class Number4 {
     public static Scanner in = new Scanner(System.in);
     public static PrintStream out = System.out;
-    static int func(int []a, int c) {
-        int sum = -c; //начинаем с -с чтобы получить сумму всех чисел кроме с
+    static double func(int []a, double c,double sum) {
+        sum-=c;
         int l = a.length-1; // берём количество эл-тов не считая выбранный
-        for ( int j = 0;j<a.length;++j)
-            sum+=a[j];
-        int sra = sum/l; //считаем среднее арифметическое
+        double sra = sum/l; //считаем среднее арифметическое
         return Math.abs(c-sra);  //возвращаем искомое значение
     }
     public static void  main(String [] args) throws IOException {
         int n  = in.nextInt();
         int []arr = new int[n];
-        for (int i = 0; i<arr.length;++i)
+        int sum = 0;
+        for (int i = 0; i<arr.length;++i) {
             arr[i] = in.nextInt();
+            sum += arr[i];
+        }
         int index = 0;     //переменная для хранения индекса искомого числа
-        int znachenie = func(arr,arr[0]);  // искомое значение, начинаем перебирать с первого эл-та
+        double znachenie = func(arr,arr[0],sum);  // искомое значение, начинаем перебирать с первого эл-та
         for (int e = 1; e<arr.length;++e)
-            if (func(arr,arr[e]) < znachenie) {  // если искомое значение текущей переменной, меньше минимально найденного
-                znachenie = func(arr,arr[e]);    // обновляем минимум и индекс
+            if (func(arr,arr[e],sum) < znachenie) {  // если искомое значение текущей переменной, меньше минимально найденного
+                znachenie = func(arr,arr[e],sum);    // обновляем минимум и индекс
                 index = e;
             }
         out.println("Значение искомого элемента:" + arr[index]);
